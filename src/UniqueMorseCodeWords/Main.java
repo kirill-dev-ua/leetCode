2cd35d1c364b362656b16acda406e87f28ec865c
observerUA <kirillkuznetsov181@gmail.com>
2025-07-27 23:24:20 +0300

    unique Morse Representations

---
+++ src/UniqueMorseCodeWords/Main.java
@@ -0,0 +1,49 @@
+package UniqueMorseCodeWords;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(uniqueMorseRepresentations(new String[]{"gin","zen","gig","msg"}));
+    }
+
+    public static int uniqueMorseRepresentations(String[] words) {
+        HashMap<Character, String> map = new HashMap<>();
+        map.put('a', ".-");
+        map.put('b', "-...");
+        map.put('c', "-.-.");
+        map.put('d', "-..");
+        map.put('e', ".");
+        map.put('f', "..-.");
+        map.put('g', "--.");
+        map.put('h', "....");
+        map.put('i', "..");
+        map.put('j', ".---");
+        map.put('k', "-.-");
+        map.put('l', ".-..");
+        map.put('m', "--");
+        map.put('n', "-.");
+        map.put('o', "---");
+        map.put('p', ".--.");
+        map.put('q', "--.-");
+        map.put('r', ".-.");
+        map.put('s', "...");
+        map.put('t', "-");
+        map.put('u', "..-");
+        map.put('v', "...-");
+        map.put('w', ".--");
+        map.put('x', "-..-");
+        map.put('y', "-.--");
+        map.put('z', "--..");
+        HashSet<String> set = new HashSet<>();
+        for(String word: words){
+            StringBuilder sb = new StringBuilder();
+            for(char ch: word.toCharArray()){
+                sb.append(map.get(ch));
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
